ce7922f54c335748590c7135a8a6f3dd388c7bb9
Mayur7862 <83858995+Mayur7862@users.noreply.github.com>
2024-07-30 20:24:12 +0530

    Updated gap in the no 1 management

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -15,9 +15,9 @@ const MarketingPage = () => {
 	return (
 		<div className="flex items-center justify-center flex-col">
 			<div className={cn("flex items-center justify-center flex-col", headingFont.className)}>
-				<div className="mb-4 flex items-center border shadow-sm gap-4 p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
+				<div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
 					<Medal className="h-6 w-6 mr-2"/>
-					Number 1 Learn Management
+					Number  1  Learn  Management
 				</div>
 				<h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">Atsatra InfoSystem</h1>
 				<div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">Work forward</div>
