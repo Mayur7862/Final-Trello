8106305195fb8500cd852bd20a880de60d386179
Mayur7862 <83858995+Mayur7862@users.noreply.github.com>
2024-07-30 20:21:49 +0530

    added gap in no 1 management

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -15,7 +15,7 @@ const MarketingPage = () => {
 	return (
 		<div className="flex items-center justify-center flex-col">
 			<div className={cn("flex items-center justify-center flex-col", headingFont.className)}>
-				<div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
+				<div className="mb-4 flex items-center border shadow-sm gap-4 p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
 					<Medal className="h-6 w-6 mr-2"/>
 					Number 1 Learn Management
 				</div>
